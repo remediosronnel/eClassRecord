a1c3207aac863ec26045faf48b9dd856d45a3993
remediosronnel <remediosronnel30@gmail.com>
2023-09-11 23:08:32 +0800

    Git Commit

--- adminapp/src/main/java/com/remedios/adminapp/MainActivity.kt
+++ adminapp/src/main/java/com/remedios/adminapp/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.remedios.adminapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.remedios.adminapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val username = binding.tvUsername1.text.toString()
+        val password = binding.tvPassword1.text.toString()
+
+        binding.loginbtn.setOnClickListener {
+            if ((username.isEmpty()) && (password.isEmpty())) {
+
+                val intent = Intent(this, UserRegisterActivity::class.java)
+                startActivity(intent)
+
+            }else{
+                Toast.makeText(this, "Invalid Entry!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
