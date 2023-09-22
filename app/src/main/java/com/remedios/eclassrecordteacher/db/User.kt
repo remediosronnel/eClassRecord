61424bcdd5b7107d5c84abf696dc3bf8f49e3278
remediosronnel <remediosronnel30@gmail.com>
2023-09-23 04:07:25 +0800

    Git Commit

--- app/src/main/java/com/remedios/eclassrecordteacher/db/User.kt
+++ app/src/main/java/com/remedios/eclassrecordteacher/db/User.kt
@@ -1,8 +1,12 @@
 package com.remedios.eclassrecordteacher.db
 
 import android.media.Image
+import androidx.room.PrimaryKey
 
 data class User(
     val userName:String,
     val image:String
-)
+){
+    @PrimaryKey(autoGenerate = true) var id:Int = 0
+
+}
--- app/src/main/java/com/remedios/eclassrecordteacher/fragment/ClassesFragment.kt
+++ app/src/main/java/com/remedios/eclassrecordteacher/fragment/ClassesFragment.kt
@@ -6,15 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.remedios.eclassrecordteacher.R
+import com.remedios.eclassrecordteacher.databinding.ActivityAddClassBinding
+import com.remedios.eclassrecordteacher.databinding.FragmentClassesBinding
 
 class ClassesFragment : Fragment() {
-
+    private var _binding:FragmentClassesBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_classes, container, false)
+        _binding = FragmentClassesBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+
+
+
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
